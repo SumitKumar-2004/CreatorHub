f8f6a7f559d886b2c0cb3cecd3dfbd77115dabbd
Sumit <skdudy546@gmail.com>
2026-05-22 22:09:52 +0530

    Utils code disccussion

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT || 8000,()=>{
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+      
+   });
+})
+.catch((err)=>{
+   console.log("Mongodb conection failed!!",err);
+   
+});
 
 /*
 import express from "express";
