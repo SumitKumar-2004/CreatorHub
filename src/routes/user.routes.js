5fb76ba7f66c5dde8b7165656f3852d3a5714f67
Sumit <skdudy546@gmail.com>
2026-05-25 20:34:43 +0530

    add refreshAccessToken endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ import { upload } from "../middlewares/multer.middleware.js";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -27,5 +28,7 @@ router.post("/register",upload.fields([
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT,logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
